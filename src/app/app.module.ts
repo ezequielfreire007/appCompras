3d6792e69b8607a121a0f2ec6e2e6bf0c88dfe9e
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2017-10-31 18:53:08 -0300

    se agrega modulo presupuesto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { Routes, RouterModule } from '@angular/router';
@@ -10,11 +11,13 @@ import { ProveedoresComponent } from './proveedores/proveedores/proveedores.comp
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
 import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import { AddpresComponent } from './presupuestos/addpres/addpres.component';
 
 const routes: Routes = [
   { path : '', component:InicioComponent },
   { path : 'proveedores', component:ProveedoresComponent },
   { path : 'addprovee', component:AddproveeComponent },
+  { path : 'addpres', component:AddpresComponent },
   { path : '**', component: InicioComponent }
 ];
 
@@ -24,11 +27,13 @@ const routes: Routes = [
     ProveedoresComponent,
     InicioComponent,
     HeaderComponent,
-    AddproveeComponent
+    AddproveeComponent,
+    AddpresComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(routes)
   ],
